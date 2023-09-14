f3f3ab7e116af22aa45005605209d63f23b65d98
Lucas <lks-matheus@hotmail.com>
2023-09-14 13:19:15 -0300

    Só pra deixar no Git

---
+++ userprofile/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Userprofile
+
+# Create your views here.
+
+def signup(request):
+    
+    if request.method == 'GET':
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+            Userprofile.objects.create(user=user)
+
+            return redirect('/login/')
+        
+    else:
+
+        form = UserCreationForm()
+
+    form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html', {
+
+        'form' : form
+
+    })
+    
